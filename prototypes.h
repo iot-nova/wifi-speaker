7dd7c771403cead9b7aa4f6dfd723b222f70e704
Valvas Vincent <valvas.vincent@outlook.fr>
2019-05-03 15:07:24 +0200

    Working version

--- prototypes.h
+++ prototypes.h
@@ -8,43 +8,13 @@
 
 #include <ESP8266Spiram.h>
 
-#include <AudioFileSource.h>
-#include <AudioFileSourceBuffer.h>
-#include <AudioFileSourceFS.h>
-#include <AudioFileSourceHTTPStream.h>
 #include <AudioFileSourceICYStream.h>
-#include <AudioFileSourceID3.h>
-#include <AudioFileSourcePROGMEM.h>
-#include <AudioFileSourceSPIFFS.h>
-#include <AudioFileSourceSPIRAMBuffer.h>
-#include <AudioFileSourceSTDIO.h>
-#include <AudioFileStream.h>
-#include <AudioGenerator.h>
-#include <AudioGeneratorAAC.h>
-#include <AudioGeneratorFLAC.h>
-#include <AudioGeneratorMIDI.h>
-#include <AudioGeneratorMOD.h>
+#include <AudioFileSourceBuffer.h>
 #include <AudioGeneratorMP3.h>
-#include <AudioGeneratorMP3a.h>
-#include <AudioGeneratorRTTTL.h>
-#include <AudioGeneratorWAV.h>
-#include <AudioOutput.h>
-#include <AudioOutputBuffer.h>
-#include <AudioOutputFilterDecimate.h>
 #include <AudioOutputI2S.h>
-#include <AudioOutputI2SNoDAC.h>
-#include <AudioOutputMixer.h>
-#include <AudioOutputNull.h>
-#include <AudioOutputSerialWAV.h>
-#include <AudioOutputSPIFFSWAV.h>
-#include <AudioOutputSTDIO.h>
-#include <AudioStatus.h>
 
-
-String webServerDisplayForm();
-String webServerHandleURLForm();
-
+void webServerDisplayForm();
+void webServerHandleURLForm();
 void playSong(String);
-void loopSong();
 
 #endif
